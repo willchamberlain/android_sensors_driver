e4fd57f72fc385309de366b870d41bfbc040cea2
Charlie Hulcher <charlie.hulcher@gmail.com>
2016-05-08 00:59:53 -0400

    Readme update. Rename to markdown format. Comment better build and run script.

--- build_and_run.sh
+++ build_and_run.sh
@@ -3,9 +3,13 @@
 ../gradlew clean assembleDebug
 if [ $? -eq 0 ]
  then
-  # -r tells it to re install
+  # -r tells it to re-install if not installed
   adb install -r build/outputs/apk/android_sensors_driver-debug.apk
+
+  # Start the app by starting its main activity
   adb shell am start -n org.ros.android.android_sensors_driver/org.ros.android.android_sensors_driver.MainActivity
+
+  # Show logs in the console
   adb logcat -c
   adb logcat
 fi
